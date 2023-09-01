5f2668b822add364265dc280f3767a77f304d2e9
Tushar <142796801+tusharsno@users.noreply.github.com>
2023-09-01 20:25:08 +0600

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,50 @@
+/*Write a Java program to create a class called "Book"
+with attributes for title, author, and ISBN and methods
+to add and remove books from a collection.*/
+
+//PART_01
+package oop_lab_work;
+
+public class Book 
+{
+    private String title;
+    private String author;
+    private int ISBN;
+    
+    public Book(String title, String author, int ISBN)
+    {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+    }
+    
+    //title
+    public String getTitle()
+    {
+       return title;
+    }
+    public void setTitle(String title)
+    { 
+        this.title = title;
+    }
+    
+    //author
+    public String getAuthor()
+    {
+        return author;
+    }
+    public void setAuthor(String author)
+    {
+        this.author = author;
+    }
+    
+    //ISBN
+    public int getISBN()
+    {
+        return ISBN;
+    }
+    public void setISBN(int ISBN)
+    {
+        this.ISBN = ISBN;
+    }
+}
